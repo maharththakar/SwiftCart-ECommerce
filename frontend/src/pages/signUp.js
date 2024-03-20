dbb4981131f7e3f2f3290de0a1bbfcdd053e6e9c
Maharth <mrthakar20012003@gmail.com>
2024-03-20 15:47:53 +0530

    included signIn and signUp pages in frontend

---
+++ frontend/src/pages/signUp.js
@@ -0,0 +1,295 @@
+import React, { useState } from "react";
+import validator from "validator";
+import { Link } from "react-router-dom";
+
+const SignUp = () => {
+  // ============= Initial State Start here =============
+  const [clientName, setClientName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [password, setPassword] = useState("");
+  const [checked, setChecked] = useState(false);
+  const [defaultaddress, setDefaultaddress] = useState("");
+  const [defaultpin, setDefaultpin] = useState("");
+  const [isValidEmail, setIsValidEmail] = useState(true); // State to track email validity
+  // ============= Initial State End here ===============
+  // ============= Error Msg Start here =================
+  const [errClientName, setErrClientName] = useState("");
+  const [errEmail, setErrEmail] = useState("");
+  const [errPhone, setErrPhone] = useState("");
+  const [errPassword, setErrPassword] = useState("");
+  const [errDefaultaddress, setErrDefaultaddress] = useState("");
+  const [errDefaultpin, setErrDefaultpin] = useState("");
+  // ============= Error Msg End here ===================
+  const [successMsg, setSuccessMsg] = useState("");
+  // ============= Event Handler Start here =============
+  const handleName = (e) => {
+    setClientName(e.target.value);
+    setErrClientName("");
+  };
+  const handleEmail = (e) => {
+    const newEmail = e.target.value;
+    setEmail(newEmail);
+
+    // Check if the entered email is valid
+    setIsValidEmail(validator.isEmail(newEmail));
+  };
+  const handlePhone = (e) => {
+    setPhone(e.target.value);
+    setErrPhone("");
+  };
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+    setErrPassword("");
+  };
+  const handleDefaultaddress = (e) => {
+    setDefaultaddress(e.target.value);
+    setErrDefaultaddress("");
+  };
+  const handleDefaultpin = (e) => {
+    setDefaultpin(e.target.value);
+    setErrDefaultpin("");
+  };
+
+  // ============= Event Handler End here ===============
+  // ================= Email Validation start here =============
+  const EmailValidation = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i);
+  };
+  // ================= Email Validation End here ===============
+
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    if (checked) {
+      if (!clientName) {
+        setErrClientName("Enter your name");
+      }
+      if (!email) {
+        setErrEmail("Enter your email");
+      } else {
+        if (!EmailValidation(email) || !isValidEmail) {
+          setErrEmail("Enter a Valid email");
+        }
+      }
+      if (!phone) {
+        setErrPhone("Enter your phone number");
+      }
+      if (!defaultaddress) {
+        setErrDefaultaddress("Enter your default address");
+      }
+      if (!defaultpin) {
+        setErrDefaultpin("Enter your default pin");
+      }
+      if (!password) {
+        setErrPassword("Create a password");
+      } else {
+        if (password.length < 6) {
+          setErrPassword("Passwords must be at least 6 characters");
+        }
+      }
+
+      // ============== Getting the value ==============
+      if (
+        clientName &&
+        email &&
+        EmailValidation(email) &&
+        password &&
+        phone &&
+        defaultaddress &&
+        defaultpin &&
+        password.length >= 8
+      ) {
+        setSuccessMsg(
+          `Hello dear ${clientName}, Welcome you to OREBI Admin panel. We received your Sign up request. We are processing to validate your access. Till then stay connected and additional assistance will be sent to you by your mail at ${email}`
+        );
+        setClientName("");
+        setEmail("");
+        setPhone("");
+        setPassword("");
+        setDefaultaddress("");
+        setDefaultpin("");
+      }
+    }
+  };
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="w-full max-w-md">
+        {successMsg ? (
+          <div className="w-[500px]">
+            <p className="w-full px-4 py-10 text-green-500 font-medium font-titleFont">
+              {successMsg}
+            </p>
+            <Link to="/signin">
+              <button
+                className="w-full h-10 bg-primeColor rounded-md text-gray-200 text-base font-titleFont font-semibold 
+            tracking-wide hover:bg-black hover:text-white duration-300"
+              >
+                Sign in
+              </button>
+            </Link>
+          </div>
+        ) : (
+          <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <div className="px-6 py-4 w-full h-[96%] flex flex-col justify-start overflow-y-scroll scrollbar-thin scrollbar-thumb-primeColor">
+              <h1 className="font-titleFont underline underline-offset-4 decoration-[1px] font-semibold text-2xl mb-4">
+                Create your account
+              </h1>
+              <div className="flex flex-col gap-3">
+                {/* client name */}
+                <div className="flex flex-col gap-.5">
+                  <p className="font-titleFont text-base font-semibold text-gray-600">
+                    Full Name
+                  </p>
+                  <input
+                    onChange={handleName}
+                    value={clientName}
+                    className="w-full h-8 placeholder:text-sm placeholder:tracking-wide px-4 text-base font-medium placeholder:font-normal rounded-md border-[1px] border-gray-400 outline-none"
+                    type="text"
+                    placeholder="eg. John Doe"
+                  />
+                  {errClientName && (
+                    <p className="text-sm text-red-500 font-titleFont font-semibold px-4">
+                      <span className="font-bold italic mr-1">!</span>
+                      {errClientName}
+                    </p>
+                  )}
+                </div>
+                {/* Email */}
+                <div className="flex flex-col gap-.5">
+                  <p className="font-titleFont text-base font-semibold text-gray-600">
+                    Email
+                  </p>
+                  <input
+                    onChange={handleEmail}
+                    value={email}
+                    className="w-full h-8 placeholder:text-sm placeholder:tracking-wide px-4 text-base font-medium placeholder:font-normal rounded-md border-[1px] border-gray-400 outline-none"
+                    type="email"
+                    placeholder="john@workemail.com"
+                  />
+                  {errEmail && (
+                    <p className="text-sm text-red-500 font-titleFont font-semibold px-4">
+                      <span className="font-bold italic mr-1">!</span>
+                      {errEmail}
+                    </p>
+                  )}
+                </div>
+                {/* Phone Number */}
+                <div className="flex flex-col gap-.5">
+                  <p className="font-titleFont text-base font-semibold text-gray-600">
+                    Phone Number
+                  </p>
+                  <input
+                    onChange={handlePhone}
+                    value={phone}
+                    className="w-full h-8 placeholder:text-sm placeholder:tracking-wide px-4 text-base font-medium placeholder:font-normal rounded-md border-[1px] border-gray-400 outline-none"
+                    type="text"
+                    placeholder="008801234567891"
+                  />
+                  {errPhone && (
+                    <p className="text-sm text-red-500 font-titleFont font-semibold px-4">
+                      <span className="font-bold italic mr-1">!</span>
+                      {errPhone}
+                    </p>
+                  )}
+                </div>
+                {/* Password */}
+                <div className="flex flex-col gap-.5">
+                  <p className="font-titleFont text-base font-semibold text-gray-600">
+                    Password
+                  </p>
+                  <input
+                    onChange={handlePassword}
+                    value={password}
+                    className="w-full h-8 placeholder:text-sm placeholder:tracking-wide px-4 text-base font-medium placeholder:font-normal rounded-md border-[1px] border-gray-400 outline-none"
+                    type="password"
+                    placeholder="Create password"
+                  />
+                  {errPassword && (
+                    <p className="text-sm text-red-500 font-titleFont font-semibold px-4">
+                      <span className="font-bold italic mr-1">!</span>
+                      {errPassword}
+                    </p>
+                  )}
+                </div>
+                {/* Default Address */}
+                <div className="flex flex-col gap-.5">
+                  <p className="font-titleFont text-base font-semibold text-gray-600">
+                    Default Address
+                  </p>
+                  <input
+                    onChange={handleDefaultaddress}
+                    value={defaultaddress}
+                    className="w-full h-8 placeholder:text-sm placeholder:tracking-wide px-4 text-base font-medium placeholder:font-normal rounded-md border-[1px] border-gray-400 outline-none"
+                    type="text"
+                    placeholder="Enter your default address"
+                  />
+                  {errDefaultaddress && (
+                    <p className="text-sm text-red-500 font-titleFont font-semibold px-4">
+                      <span className="font-bold italic mr-1">!</span>
+                      {errDefaultaddress}
+                    </p>
+                  )}
+                </div>
+                {/* Default Pin */}
+                <div className="flex flex-col gap-.5">
+                  <p className="font-titleFont text-base font-semibold text-gray-600">
+                    Default Pin
+                  </p>
+                  <input
+                    onChange={handleDefaultpin}
+                    value={defaultpin}
+                    className="w-full h-8 placeholder:text-sm placeholder:tracking-wide px-4 text-base font-medium placeholder:font-normal rounded-md border-[1px] border-gray-400 outline-none"
+                    type="text"
+                    placeholder="Enter your default pin"
+                  />
+                  {errDefaultpin && (
+                    <p className="text-sm text-red-500 font-titleFont font-semibold px-4">
+                      <span className="font-bold italic mr-1">!</span>
+                      {errDefaultpin}
+                    </p>
+                  )}
+                </div>
+
+                {/* Checkbox */}
+                <div className="flex items-start mdl:items-center gap-2">
+                  <input
+                    onChange={() => setChecked(!checked)}
+                    className="w-4 h-4 mt-1 mdl:mt-0 cursor-pointer"
+                    type="checkbox"
+                  />
+                  <p className="text-sm text-primeColor">
+                    I agree to the OREBI{" "}
+                    <span className="text-blue-500">Terms of Service </span>and{" "}
+                    <span className="text-blue-500">Privacy Policy</span>.
+                  </p>
+                </div>
+                <button
+                  onClick={handleSignUp}
+                  className={`${
+                    checked
+                      ? "bg-primeColor hover:bg-black hover:text-white cursor-pointer"
+                      : "bg-gray-500 hover:bg-gray-500 hover:text-gray-200 cursor-none"
+                  } w-full text-gray-200 text-base font-medium h-10 rounded-md hover:text-white duration-300`}
+                >
+                  Create Account
+                </button>
+                <p className="text-sm text-center font-titleFont font-medium">
+                  Don't have an Account?{" "}
+                  <Link to="/signin">
+                    <span className="hover:text-blue-600 duration-300">
+                      Sign in
+                    </span>
+                  </Link>
+                </p>
+              </div>
+            </div>
+          </form>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
